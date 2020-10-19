7a37560f2ded7472a9fff5c51179527bc8638308
Christopher Hertel <mail@christopher-hertel.de>
2020-10-19 18:48:57 +0200

    moving to cli-note

---
+++ code/TestClass.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+class TestClass
+{
+    public function helloWorld(): void
+    {
+        // Comment
+        echo 'Hello World!';
+    }
+}
